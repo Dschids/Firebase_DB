b6b0658a2682db976e1faf4b7f173ed2e0536db7
Bob Dschida <rdschida@students.mchenry.edu>
2024-03-21 15:53:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/firebasedb/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.firebasedb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        database = Firebase.database.reference
+        
+    }
+}
